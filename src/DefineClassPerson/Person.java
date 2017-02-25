04c2796699a7daf8d2a8760b7354b75227618b29
Ivan Vachev <Ivan Vachev>
2017-02-25 22:46:47 +0200

    Add OOP

---
+++ src/DefineClassPerson/Person.java
@@ -0,0 +1,20 @@
+package DefineClassPerson;
+
+public class Person {
+
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+}
